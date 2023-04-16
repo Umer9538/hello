aea7bef05ca07dca10ff5d219ec79127c383c6ba
Muhammad's Mac <mac@macs-MacBook-Pro-3.local>
2023-04-16 18:33:18 +0500

    Implement auth_repository_65

---
+++ lib/auth_repository_65.dart
@@ -0,0 +1,38 @@
+// Auto-generated file: auth_repository_65.dart
+
+class AuthRepository65 {
+  final String id;
+  final String name;
+  final DateTime createdAt;
+
+  AuthRepository65({
+    required this.id,
+    required this.name,
+    required this.createdAt,
+  });
+
+  factory AuthRepository65.fromJson(Map<String, dynamic> json) {
+    return AuthRepository65(
+      id: json['id'] as String,
+      name: json['name'] as String,
+      createdAt: DateTime.parse(json['createdAt'] as String),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'createdAt': createdAt.toIso8601String(),
+    };
+  }
+
+  void process() {
+    print('Processing $name');
+  }
+
+  Future<void> execute() async {
+    await Future.delayed(const Duration(milliseconds: 100));
+    print('Executed $id');
+  }
+}
