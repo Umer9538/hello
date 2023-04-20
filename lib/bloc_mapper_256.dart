da3ca1e417f95ee0c0e4d6c41c82b062f5c10b60
Muhammad's Mac <mac@macs-MacBook-Pro-3.local>
2023-04-20 08:08:44 +0500

    Add bloc mapper 256

---
+++ lib/bloc_mapper_256.dart
@@ -0,0 +1,38 @@
+// Auto-generated file: bloc_mapper_256.dart
+
+class BlocMapper256 {
+  final String id;
+  final String name;
+  final DateTime createdAt;
+
+  BlocMapper256({
+    required this.id,
+    required this.name,
+    required this.createdAt,
+  });
+
+  factory BlocMapper256.fromJson(Map<String, dynamic> json) {
+    return BlocMapper256(
+      id: json['id'] as String,
+      name: json['name'] as String,
+      createdAt: DateTime.parse(json['createdAt'] as String),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'createdAt': createdAt.toIso8601String(),
+    };
+  }
+
+  void process() {
+    print('Processing $name');
+  }
+
+  Future<void> execute() async {
+    await Future.delayed(const Duration(milliseconds: 100));
+    print('Executed $id');
+  }
+}
