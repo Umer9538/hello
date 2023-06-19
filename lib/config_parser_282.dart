5d39f301ad697e4167d31b5699f1011b16d7f10b
Muhammad's Mac <mac@macs-MacBook-Pro-3.local>
2023-06-19 21:49:18 +0500

    Create config parser 282 class

---
+++ lib/config_parser_282.dart
@@ -0,0 +1,38 @@
+// Auto-generated file: config_parser_282.dart
+
+class ConfigParser282 {
+  final String id;
+  final String name;
+  final DateTime createdAt;
+
+  ConfigParser282({
+    required this.id,
+    required this.name,
+    required this.createdAt,
+  });
+
+  factory ConfigParser282.fromJson(Map<String, dynamic> json) {
+    return ConfigParser282(
+      id: json['id'] as String,
+      name: json['name'] as String,
+      createdAt: DateTime.parse(json['createdAt'] as String),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'createdAt': createdAt.toIso8601String(),
+    };
+  }
+
+  void process() {
+    print('Processing $name');
+  }
+
+  Future<void> execute() async {
+    await Future.delayed(const Duration(milliseconds: 100));
+    print('Executed $id');
+  }
+}
