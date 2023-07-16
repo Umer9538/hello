12bf2a4a3e215d389f8b39f8f875542b3cca966f
Muhammad's Mac <mac@macs-MacBook-Pro-3.local>
2023-07-16 16:34:33 +0500

    feat: add dashboard_cubit_33

---
+++ lib/dashboard_cubit_33.dart
@@ -0,0 +1,38 @@
+// Auto-generated file: dashboard_cubit_33.dart
+
+class DashboardCubit33 {
+  final String id;
+  final String name;
+  final DateTime createdAt;
+
+  DashboardCubit33({
+    required this.id,
+    required this.name,
+    required this.createdAt,
+  });
+
+  factory DashboardCubit33.fromJson(Map<String, dynamic> json) {
+    return DashboardCubit33(
+      id: json['id'] as String,
+      name: json['name'] as String,
+      createdAt: DateTime.parse(json['createdAt'] as String),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'createdAt': createdAt.toIso8601String(),
+    };
+  }
+
+  void process() {
+    print('Processing $name');
+  }
+
+  Future<void> execute() async {
+    await Future.delayed(const Duration(milliseconds: 100));
+    print('Executed $id');
+  }
+}
