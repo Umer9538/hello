3d9ece7eae9a59128a511681c744c090d1194544
Muhammad's Mac <mac@macs-MacBook-Pro-3.local>
2023-07-16 16:52:17 +0500

    Create data factory 4 class

---
+++ lib/data_factory_4.dart
@@ -0,0 +1,38 @@
+// Auto-generated file: data_factory_4.dart
+
+class DataFactory4 {
+  final String id;
+  final String name;
+  final DateTime createdAt;
+
+  DataFactory4({
+    required this.id,
+    required this.name,
+    required this.createdAt,
+  });
+
+  factory DataFactory4.fromJson(Map<String, dynamic> json) {
+    return DataFactory4(
+      id: json['id'] as String,
+      name: json['name'] as String,
+      createdAt: DateTime.parse(json['createdAt'] as String),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'createdAt': createdAt.toIso8601String(),
+    };
+  }
+
+  void process() {
+    print('Processing $name');
+  }
+
+  Future<void> execute() async {
+    await Future.delayed(const Duration(milliseconds: 100));
+    print('Executed $id');
+  }
+}
