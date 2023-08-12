ad7aaf5a7353822a5581ffed8aa7c48c96875339
Muhammad's Mac <mac@macs-MacBook-Pro-3.local>
2023-08-12 14:20:19 +0500

    Create feed event 182 class

---
+++ lib/feed_event_182.dart
@@ -0,0 +1,38 @@
+// Auto-generated file: feed_event_182.dart
+
+class FeedEvent182 {
+  final String id;
+  final String name;
+  final DateTime createdAt;
+
+  FeedEvent182({
+    required this.id,
+    required this.name,
+    required this.createdAt,
+  });
+
+  factory FeedEvent182.fromJson(Map<String, dynamic> json) {
+    return FeedEvent182(
+      id: json['id'] as String,
+      name: json['name'] as String,
+      createdAt: DateTime.parse(json['createdAt'] as String),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'createdAt': createdAt.toIso8601String(),
+    };
+  }
+
+  void process() {
+    print('Processing $name');
+  }
+
+  Future<void> execute() async {
+    await Future.delayed(const Duration(milliseconds: 100));
+    print('Executed $id');
+  }
+}
