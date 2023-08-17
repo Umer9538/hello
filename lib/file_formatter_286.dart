5c2eb7dc3b87da232e68384f1d6a3a9071bc23c5
Muhammad's Mac <mac@macs-MacBook-Pro-3.local>
2023-08-17 22:27:15 +0500

    Add new file formatter 286 functionality

---
+++ lib/file_formatter_286.dart
@@ -0,0 +1,38 @@
+// Auto-generated file: file_formatter_286.dart
+
+class FileFormatter286 {
+  final String id;
+  final String name;
+  final DateTime createdAt;
+
+  FileFormatter286({
+    required this.id,
+    required this.name,
+    required this.createdAt,
+  });
+
+  factory FileFormatter286.fromJson(Map<String, dynamic> json) {
+    return FileFormatter286(
+      id: json['id'] as String,
+      name: json['name'] as String,
+      createdAt: DateTime.parse(json['createdAt'] as String),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'createdAt': createdAt.toIso8601String(),
+    };
+  }
+
+  void process() {
+    print('Processing $name');
+  }
+
+  Future<void> execute() async {
+    await Future.delayed(const Duration(milliseconds: 100));
+    print('Executed $id');
+  }
+}
