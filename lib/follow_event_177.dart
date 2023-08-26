3e295be7b57817fdeefac8fe3f180815d20d1961
Muhammad's Mac <mac@macs-MacBook-Pro-3.local>
2023-08-26 20:55:17 +0500

    Add new follow event 177 functionality

---
+++ lib/follow_event_177.dart
@@ -0,0 +1,38 @@
+// Auto-generated file: follow_event_177.dart
+
+class FollowEvent177 {
+  final String id;
+  final String name;
+  final DateTime createdAt;
+
+  FollowEvent177({
+    required this.id,
+    required this.name,
+    required this.createdAt,
+  });
+
+  factory FollowEvent177.fromJson(Map<String, dynamic> json) {
+    return FollowEvent177(
+      id: json['id'] as String,
+      name: json['name'] as String,
+      createdAt: DateTime.parse(json['createdAt'] as String),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'createdAt': createdAt.toIso8601String(),
+    };
+  }
+
+  void process() {
+    print('Processing $name');
+  }
+
+  Future<void> execute() async {
+    await Future.delayed(const Duration(milliseconds: 100));
+    print('Executed $id');
+  }
+}
