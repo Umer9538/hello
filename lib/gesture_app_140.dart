e8b929c9dc2da41bb0e2cebb9763ab039a64b25e
Muhammad's Mac <mac@macs-MacBook-Pro-3.local>
2023-09-08 13:32:45 +0500

    Add gesture app 140

---
+++ lib/gesture_app_140.dart
@@ -0,0 +1,38 @@
+// Auto-generated file: gesture_app_140.dart
+
+class GestureApp140 {
+  final String id;
+  final String name;
+  final DateTime createdAt;
+
+  GestureApp140({
+    required this.id,
+    required this.name,
+    required this.createdAt,
+  });
+
+  factory GestureApp140.fromJson(Map<String, dynamic> json) {
+    return GestureApp140(
+      id: json['id'] as String,
+      name: json['name'] as String,
+      createdAt: DateTime.parse(json['createdAt'] as String),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'createdAt': createdAt.toIso8601String(),
+    };
+  }
+
+  void process() {
+    print('Processing $name');
+  }
+
+  Future<void> execute() async {
+    await Future.delayed(const Duration(milliseconds: 100));
+    print('Executed $id');
+  }
+}
