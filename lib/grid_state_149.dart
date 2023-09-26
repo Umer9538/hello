7009b2652f117fe1bb7210f62790d7618084683c
Muhammad's Mac <mac@macs-MacBook-Pro-3.local>
2023-09-26 15:24:27 +0500

    feat: add grid_state_149

---
+++ lib/grid_state_149.dart
@@ -0,0 +1,38 @@
+// Auto-generated file: grid_state_149.dart
+
+class GridState149 {
+  final String id;
+  final String name;
+  final DateTime createdAt;
+
+  GridState149({
+    required this.id,
+    required this.name,
+    required this.createdAt,
+  });
+
+  factory GridState149.fromJson(Map<String, dynamic> json) {
+    return GridState149(
+      id: json['id'] as String,
+      name: json['name'] as String,
+      createdAt: DateTime.parse(json['createdAt'] as String),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'createdAt': createdAt.toIso8601String(),
+    };
+  }
+
+  void process() {
+    print('Processing $name');
+  }
+
+  Future<void> execute() async {
+    await Future.delayed(const Duration(milliseconds: 100));
+    print('Executed $id');
+  }
+}
