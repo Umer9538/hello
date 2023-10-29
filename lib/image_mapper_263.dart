9027cd3d58364bfa1d850368ab0e626d51fa0ccb
Muhammad's Mac <mac@macs-MacBook-Pro-3.local>
2023-10-29 14:04:40 +0500

    Add new image mapper 263 functionality

---
+++ lib/image_mapper_263.dart
@@ -0,0 +1,38 @@
+// Auto-generated file: image_mapper_263.dart
+
+class ImageMapper263 {
+  final String id;
+  final String name;
+  final DateTime createdAt;
+
+  ImageMapper263({
+    required this.id,
+    required this.name,
+    required this.createdAt,
+  });
+
+  factory ImageMapper263.fromJson(Map<String, dynamic> json) {
+    return ImageMapper263(
+      id: json['id'] as String,
+      name: json['name'] as String,
+      createdAt: DateTime.parse(json['createdAt'] as String),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'createdAt': createdAt.toIso8601String(),
+    };
+  }
+
+  void process() {
+    print('Processing $name');
+  }
+
+  Future<void> execute() async {
+    await Future.delayed(const Duration(milliseconds: 100));
+    print('Executed $id');
+  }
+}
