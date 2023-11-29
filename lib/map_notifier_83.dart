6d12e1bd9c8052a3417f621a4405c5460f01f768
Muhammad's Mac <mac@macs-MacBook-Pro-3.local>
2023-11-29 10:48:42 +0500

    Create map notifier 83 class

---
+++ lib/map_notifier_83.dart
@@ -0,0 +1,38 @@
+// Auto-generated file: map_notifier_83.dart
+
+class MapNotifier83 {
+  final String id;
+  final String name;
+  final DateTime createdAt;
+
+  MapNotifier83({
+    required this.id,
+    required this.name,
+    required this.createdAt,
+  });
+
+  factory MapNotifier83.fromJson(Map<String, dynamic> json) {
+    return MapNotifier83(
+      id: json['id'] as String,
+      name: json['name'] as String,
+      createdAt: DateTime.parse(json['createdAt'] as String),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'createdAt': createdAt.toIso8601String(),
+    };
+  }
+
+  void process() {
+    print('Processing $name');
+  }
+
+  Future<void> execute() async {
+    await Future.delayed(const Duration(milliseconds: 100));
+    print('Executed $id');
+  }
+}
