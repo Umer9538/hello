ccd048633918f4771552bd01dfe42fa6b571ada4
Muhammad's Mac <mac@macs-MacBook-Pro-3.local>
2023-11-30 11:21:43 +0500

    Add menu handler 86

---
+++ lib/menu_handler_86.dart
@@ -0,0 +1,38 @@
+// Auto-generated file: menu_handler_86.dart
+
+class MenuHandler86 {
+  final String id;
+  final String name;
+  final DateTime createdAt;
+
+  MenuHandler86({
+    required this.id,
+    required this.name,
+    required this.createdAt,
+  });
+
+  factory MenuHandler86.fromJson(Map<String, dynamic> json) {
+    return MenuHandler86(
+      id: json['id'] as String,
+      name: json['name'] as String,
+      createdAt: DateTime.parse(json['createdAt'] as String),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'createdAt': createdAt.toIso8601String(),
+    };
+  }
+
+  void process() {
+    print('Processing $name');
+  }
+
+  Future<void> execute() async {
+    await Future.delayed(const Duration(milliseconds: 100));
+    print('Executed $id');
+  }
+}
